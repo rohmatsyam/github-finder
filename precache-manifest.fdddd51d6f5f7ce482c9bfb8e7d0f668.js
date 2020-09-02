0d003d89db921e7d06eacdd2e1e253e68530e048
rohmatsyam <rohmatsyam@yahoo.com>
2020-09-02 14:46:49 +0700

    Updates

---
+++ precache-manifest.fdddd51d6f5f7ce482c9bfb8e7d0f668.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d39df8309637153914bcc988b1c4185",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "aaccc41ddae1e23042dd",
+    "url": "/github-finder/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "3065a75146871b98a0e1",
+    "url": "/github-finder/static/js/2.03818c25.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder/static/js/2.03818c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaccc41ddae1e23042dd",
+    "url": "/github-finder/static/js/main.8043904c.chunk.js"
+  },
+  {
+    "revision": "189fbc883acbcb16644e",
+    "url": "/github-finder/static/js/runtime-main.c7dda347.js"
+  }
+]);
